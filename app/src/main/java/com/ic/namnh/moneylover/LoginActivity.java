fc8b421f5140872d383ba844ea4e5d200a0ae6d6
nam4chedspi <namnh.ict@gmail.com>
2015-11-06 12:36:44 +0900

    la la la

---
+++ app/src/main/java/com/ic/namnh/moneylover/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.ic.namnh.moneylover;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    Button registerButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        registerButton = (Button) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(in);
+            }
+        });
+    }
+
+}
